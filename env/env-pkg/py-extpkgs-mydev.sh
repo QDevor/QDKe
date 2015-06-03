2c2386f1aa9616ff54ef02debbf378a1e6235251
QDevor <QDevor@163.com>
2015-06-03 17:28:32 +0800

    dirty

--- env/env-pkg/py-extpkgs-mydev.sh
+++ env/env-pkg/py-extpkgs-mydev.sh
@@ -30,47 +30,26 @@ export PYTHON=python2
 . $PROGDIR/../env-msys2/qdev-build-common.sh
 . $PROGDIR/../env-pkg/py-qstk-common.sh
 #----------------------------------------
-
-#numpy-1.9.2+mkl-cp27-none-win32.whl
-#wget -q -O- 'http://www.lfd.uci.edu/~gohlke/pythonlibs/' | grep -i '\>numpy&#8209*.whl\<\/a\>'
-extpkgs_uwb_findPkgByName() {
-	pkg_nam=$1
-	pkgverrel=`wget -q -O- ''$extpkgs_uwb_url'/' | \
-			grep -i '>'$pkg_nam'&#8209;.*whl' | \
-			sed 's/&#8209;/-/g' | \
-			sed -n 's,.*'$pkg_nam'-\([0-9\.]*-.*\)\.whl.*,\1,p' | \
-			head -1`
-	pkg_ffile=$pkg_nam-$pkgverrel.whl
-	echo $pkg_ffile
-}
-
-extpkgs_uwb_getPkg() {
-	pkg_url=$extpkgs_uwb_dnl/$pkg_ffile
-	cd cd $qdev_build_top || die
-	loop_curl $pkg_ffile $pkg_url
-}
-
-extpkgs_uwb_installPkg() {
-	qdev_set					$work_home $user_name $apps_name $apps_more
-	cd cd $qdev_build_top || die
-	extpkgs_uwb_findPkgByName $apps_name
-	extpkgs_uwb_getPkg
-	
-	$PIP install $pkg_ffile || die
+. $PROGDIR/../env-pkg/py-extpkgs-UnofficialWindowsBinaries.sh
+#----------------------------------------
+qdev_try() {
+	apps_name=numpy && extpkgs_uwb_installPkg || die
+	apps_name=scipy && extpkgs_uwb_installPkg || die
+	apps_name=pandas && extpkgs_uwb_installPkg || die
 }
 
 #
 # Required and optional software
 #
 #----------------------------------------
-extpkgs_uwb_url=http://www.lfd.uci.edu/~gohlke/pythonlibs
-extpkgs_uwb_dnl=$extpkgs_uwb_url/68tmfkay
-extpkgs_uwb_pkg=
+#extpkgs_uwb_url=http://www.lfd.uci.edu/~gohlke/pythonlibs
+#extpkgs_uwb_dnl=$extpkgs_uwb_url/68tmfkay
+#extpkgs_uwb_pkg=
 #----------------------------------------
 work_home=$QSTK_WORK_HOME
 user_name=extpkgs_uwb
-apps_name=
-apps_more=
+#apps_name=
+#apps_more=
 #----------------------------------------
 # pkg_nam=?
 # pkg_typ=?
@@ -79,9 +58,9 @@ apps_more=
 # pkg_url=?
 #----------------------------------------
 qdev_init
-qdev_set					$work_home $user_name $apps_name $apps_more
+#qdev_set					$work_home $user_name $apps_name $apps_more
 #qdev_setmore
 #qdev_get
 #qdev_check
-#qdev_try
+qdev_try
 #qdev_tst
