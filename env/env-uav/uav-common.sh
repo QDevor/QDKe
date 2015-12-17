cd60506224f09ae84db67c0b9092e25f359ded43
QDevor <QDevor@163.com>
2015-12-17 13:52:13 +0100

    ...

---
+++ env/env-uav/uav-common.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#
+#            Copyright (C) 2015 QDevor
+#
+#  Licensed under the GNU General Public License, Version 3.0 (the License);
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#            http://www.gnu.org/licenses/gpl-3.0.html
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#----------------RUN-ONCE----------------
+if [[ x$INCLUDE_UAV_COMMON_SCRIPT == "xtrue" ]]; then
+	echo [QDKe] - We Are Checking Included More Than Once - uav-common.sh.
+else
+#----------------RUN-ONCE----------------
+
+PROGDIR=`dirname $0`
+PROGDIR=`cd $PROGDIR && pwd -P`
+
+# echo [Debug] - '$0'=$0
+FILENAME=`basename $0`
+PROGTYPE=${FILENAME#*.}
+PROGNAME=${FILENAME%.*}
+# echo [Debug] - The script is: $PROGDIR/$PROGNAME.$PROGTYPE
+#----------------------------------------
+export PYTHON=python2
+#----------------------------------------
+. $PROGDIR/../env-msys2/entry-common.sh
+. $PROGDIR/../env-msys2/qdev-build-common.sh
+#----------------------------------------
+for filename in `ls $QDKE_ROOT/env/env-uav/`
+do
+    if [ -f $PROGDIR/../env-uav/$filename ]; then
+      case $filename in
+        uav-fn-*.sh)
+          echo [debug] Will included $PROGDIR/../env-uav/$filename
+          . $PROGDIR/../env-uav/$filename
+          ;;
+        *) 
+          continue;;
+      esac
+    fi
+done
+#----------------------------------------
+
+
+uav_common_init() {
+	
+	return 0
+}
+
+uav_common_conf() {
+	qdev_build_config $@
+	return 0
+}
+
+uav_common_make() {
+	qdev_build_make $@
+	return 0
+}
+#----------------------------------------
+# uav_common_init
+# uav_common_conf
+# uav_common_make
+#----------------RUN-ONCE----------------
+export INCLUDE_UAV_COMMON_SCRIPT=true
+fi
+#----------------RUN-ONCE----------------
---
+++ env/env-uav/uav-paparazzi-paparazzi.bat
@@ -0,0 +1,51 @@
+rem.
+rem            Copyright (C) 2015 QDevor
+rem.
+rem  Licensed under the GNU General Public License, Version 3.0 (the License);
+rem  you may not use this file except in compliance with the License.
+rem  You may obtain a copy of the License at
+rem.
+rem            http://www.gnu.org/licenses/gpl-3.0.html
+rem.
+rem Unless required by applicable law or agreed to in writing, software
+rem distributed under the License is distributed on an AS IS BASIS,
+rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem See the License for the specific language governing permissions and
+rem limitations under the License.
+rem.
+
+:: Checking QDKe Env again but not really
+:----------------------------------------
+echo [QDKe] - We Are Checking QDKe Env.
+call env/env-win/entry-common.bat
+:----------------------------------------
+call env/env-uav/uav-common.bat
+:----------------------------------------
+set "PGM_BATCH_FILE=%~n0"
+set "PGM_BATCH_FILE=!PGM_BATCH_FILE:~3!"
+rem for /f "tokens=x,y,m-n delims=chars" %%a in ("str")   do cmd
+for /f "tokens=1* delims=-" %%a in ("!PGM_BATCH_FILE!") do (
+	set PGM_USER=%%a
+	set PGM_NAME=%%b
+)
+set "PGM_HOST=github.com"
+set "PGM_WORK_HOME=!QDKE_HOME!/uav_home/!PGM_USER!/!PGM_NAME!"
+set "PGM_WORK_HOME=!PGM_WORK_HOME:/=\!"
+:----------------------------------------
+echo [Building][Go] - Starting  - !PGM_BATCH_FILE!.
+:----------------------------------------
+rem git clone git://git.openpilot.org/OpenPilot.git OpenPilot
+:----------------------------------------
+echo [Building][Go] - Makeing  - !PGM_USER!/!PGM_NAME!.
+if not exist !PGM_NAME!-patch-stamp (
+	echo [Building][Go] - Makeing  - !PGM_USER!/!PGM_NAME!.
+  bash -- login -i -c "../env/env-uav/uav-!PGM_USER!-!PGM_NAME!.sh"
+)
+:----------------------------------------
+
+:----------------------------------------
+echo [Building][Go] - Ending    - !PGM_BATCH_FILE!.
+:----------------RUN-ONCE----------------
+set INCLUDE_GO_INSTRANCE_BATCH=true
+:EOF
+:----------------RUN-ONCE----------------
